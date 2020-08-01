573b4672763bf1c132a7048610c28f1860391cbb
LeonNimoy <leonardo.msc42@gmail.com>
2020-08-01 19:25:15 -0300

    Building the calculator component.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import ClientCalculator from './components/client_calculator'
+function App() {
+  return (
+    <div>
+    <ClientCalculator/>
+    </div>
+    );
+}
+
+export default App;
