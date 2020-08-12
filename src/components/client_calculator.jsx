93f161a5b2abbf98a275c2774dd0c9cba700635b
LeonNimoy <leonardo.msc42@gmail.com>
2020-08-12 20:44:53 -0300

    React-hook; yup; toastify added

--- src/components/client_calculator.jsx
+++ src/components/client_calculator.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers';
+import * as yup from 'yup';
+import { toast } from 'react-toastify';
 
 import {
   CalculatorContainer,
@@ -8,6 +12,44 @@ import {
   PercentageContainer,
   CalculateButton,
 } from './client_calculator.style';
+
+// function calculateBill() {
+//   const billAmt = document.getElementById('billamt').value;
+//   const garcomPerc = document.getElementById('garcomPerc').value;
+//   let numOfPeople = document.getElementById('peopleamt').value;
+
+//   // validate input
+//   if (billAmt === '' || garcomPerc == 0) {
+//     alert('Please enter values');
+//     return;
+//   }
+//   // Check to see if this input is empty or less than or equal to 1
+//   if (numOfPeople === '' || numOfPeople <= 1) {
+//     numOfPeople = 1;
+//     document.getElementById('each').style.display = 'none';
+//   } else {
+//     document.getElementById('each').style.display = 'block';
+//   }
+
+//   // Calculate tip
+//   let total = (Number(billAmt) + billAmt * garcomPerc) / numOfPeople;
+//   // round to two decimal places
+//   // total = Math.round(total * 100) / 100;
+//   // next line allows us to always have two digits after decimal point
+//   total = total.toFixed(2);
+//   // Display the tip
+//   document.getElementById('totalBill').style.display = 'block';
+//   document.getElementById('tip').innerHTML = total;
+// }
+
+// // Hide the tip amount on load
+// document.getElementById('totalBill').style.display = 'none';
+// document.getElementById('each').style.display = 'none';
+
+// // click to call function
+// document.getElementById('calculate').onclick = function () {
+//   calculateBill();
+// };
 
 function ClientCalculator() {
   return (
