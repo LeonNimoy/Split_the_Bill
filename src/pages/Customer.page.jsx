cbf86bd41c2f797a2d2e7eb336c92778648f8a05
LeonNimoy <leonardo.msc42@gmail.com>
2020-08-14 21:43:48 -0300

    Customer page upgraded

---
+++ src/pages/Customer.page.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import CustomerCalculator from '../components/CustomerCalculator.component';
+
+const Customer = () => <CustomerCalculator />;
+
+export default Customer;
