195ae74c0cf6d2b39d14ca43ba1f4e1fcd262c73
LeonNimoy <leonardo.msc42@gmail.com>
2020-08-14 22:09:47 -0300

    [wip]App file upgraded

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
 import React from 'react';
 import { ToastContainer } from 'react-toastify';
-import './App.css';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.css';
+// import './App.css';
 
 import Routes from './routes/Routes';
 
-function App() {
-  return (
-    <>
-      <ToastContainer
-        position="top-center"
-        pauseOnHover="true"
-        autoClose={5000}
-      />
-      <Routes />
-    </>
-  );
-}
+const App = () => (
+  <>
+    <ToastContainer
+      position="top-center"
+      pauseOnHover="true"
+      autoClose={5000}
+    />
+    <Routes />
+  </>
+);
 
 export default App;
